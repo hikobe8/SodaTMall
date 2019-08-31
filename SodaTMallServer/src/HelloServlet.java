6879bbfdf6c2a5d9029d16eae8dd74886410933c
hikobe8 <hikobe8@163.com>
2019-08-31 23:58:48 +0800

    init web project

---
+++ SodaTMallServer/src/HelloServlet.java
@@ -0,0 +1,26 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.Date;
+
+/**
+ * Author : Ray
+ * Created At : 2019-08-31 11:20 PM
+ * Email : ryu18356@gmail.com
+ * Description :
+ */
+public class HelloServlet extends HttpServlet{
+
+    @Override
+    protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        try {
+            resp.getWriter().println("<h1>Hello Servlet!</h1>");
+            resp.getWriter().println(new Date().toLocaleString());
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
