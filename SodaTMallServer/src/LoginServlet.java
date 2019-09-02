e3e02d4d3e585ecf148deef2543707a120af67de
hikobe8 <hikobe8@163.com>
2019-09-02 20:30:37 +0800

    jump by server and client

--- SodaTMallServer/src/LoginServlet.java
+++ SodaTMallServer/src/LoginServlet.java
@@ -17,13 +17,16 @@ public class LoginServlet extends HttpServlet {
         resp.setContentType("text/html; charset=UTF-8");
         String name = req.getParameter("name");
         String password = req.getParameter("password");
-        StringBuilder responseBuilder = new StringBuilder();
         if ("admin".equals(name) && "123".equals(password)) {
-            responseBuilder.append("<h1 style='color:green'>").append(name).append(", 你好").append("</h1>");
+            //服务端跳转
+//            req.getRequestDispatcher("success.html").forward(req, resp);
+            //客户端跳转
+            resp.sendRedirect("success.html");
         } else {
-            responseBuilder.append("<h1 style='color:red'>").append("用户名或密码错误!").append("</h1>");
+//            req.getRequestDispatcher("failure.html").forward(req, resp);
+            resp.sendRedirect("failure.html");
         }
-        resp.getWriter().println(responseBuilder.toString());
+
     }
 
 }
